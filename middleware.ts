0d564fdfff5c133a193c530587171b698c652bbd
AlphaRora <adoba@alpharora.com>
2024-06-12 02:34:40 +0000

    tanl

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,25 @@
-import NextAuth from 'next-auth';
-import { authConfig } from 'app/auth.config';
+// import NextAuth from 'next-auth';
+// import { authConfig } from 'app/auth.config';
 
-export default NextAuth(authConfig).auth;
+// export default NextAuth(authConfig).auth;
+
+// export const config = {
+//   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+//   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+// };
+
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
