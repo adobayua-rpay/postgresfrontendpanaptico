b9cffdbf1f5453891734e5a9e7fb58c53dd35b9c
AlphaRora <adoba@alpharora.com>
2024-06-12 16:55:06 +0000

    sink

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,8 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
 import Sidebar from './sidebar'
+import UserProfile from '../user-profile/page'
+
 export default function Page() {
   return (
-    <Sidebar />
+    <UserProfile />
     // <main className="flex min-h-screen w-full items-center justify-between">
     //   <div className="mx-auto">
     //     <SignIn />
