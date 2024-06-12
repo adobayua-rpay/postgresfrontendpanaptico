db39949fa92bdf5c208e65784458c2cd65ad0d91
AlphaRora <adoba@alpharora.com>
2024-06-12 03:19:55 +0000

    tanl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,62 +1,30 @@
-// import './globals.css';
+import './globals.css';
 
-// import { GeistSans } from 'geist/font/sans';
+import { GeistSans } from 'geist/font/sans';
 
-// let title = 'Next.js + Postgres Auth Starter';
-// let description =
-//   'This is a Next.js starter kit that uses NextAuth.js for simple email + password login and a Postgres database to persist the data.';
+let title = 'Next.js + Postgres Auth Starter';
+let description =
+  'This is a Next.js starter kit that uses NextAuth.js for simple email + password login and a Postgres database to persist the data.';
 
-// export const metadata = {
-//   title,
-//   description,
-//   twitter: {
-//     card: 'summary_large_image',
-//     title,
-//     description,
-//   },
-//   metadataBase: new URL('https://nextjs-postgres-auth.vercel.app'),
-// };
+export const metadata = {
+  title,
+  description,
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+  },
+  metadataBase: new URL('https://nextjs-postgres-auth.vercel.app'),
+};
 
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <body className={GeistSans.variable}>{children}</body>
-//     </html>
-//   );
-// }
-
-
-
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+    <html lang="en">
+      <body className={GeistSans.variable}>{children}</body>
+    </html>
+  );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,3 @@
-// import NextAuth from 'next-auth';
-// import { authConfig } from 'app/auth.config';
-
-// export default NextAuth(authConfig).auth;
-
-// export const config = {
-//   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-//   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-// };
-
-// import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// export default clerkMiddleware();
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
